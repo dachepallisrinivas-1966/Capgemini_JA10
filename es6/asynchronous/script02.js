2606ee05b33e95780843c258bf0a25c254c47bf6
dachepalli srinivas <dachepallisrinivas@gmail.com>
2021-05-12 08:39:25 +0530

    es6 updated

--- es6/asynchronous/script02.js
+++
@@ -1,8 +0,0 @@
-
-greet = function(userName="Srinu"){
-    console.log("Hello " + userName);
-}
-
-setInterval(() => {greet("Vamsy");},2000);
-
-console.log("Program Terminated");
--- es6/p3.js
+++
@@ -1,7 +0,0 @@
-a = 10;
-console.log(a);
-a = "Srinivas";
-console.log(a);
-a = 2.5;
-console.log(a);
-
